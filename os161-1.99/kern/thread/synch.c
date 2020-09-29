1c7ea4be152d2c973738589dd7dd01cc19904701
susachen <s529chen@uwaterloo.ca>
2020-09-28 21:15:20 -0400

    lock hold

--- os161-1.99/kern/thread/synch.c
+++ os161-1.99/kern/thread/synch.c
@@ -217,11 +217,8 @@ lock_release(struct lock *lock)
 bool
 lock_do_i_hold(struct lock *lock)
 {
-        // Write this
-
-        (void)lock;  // suppress warning until code gets written
-
-        return true; // dummy until code gets written
+        if (lock->owner == curthread) return true;
+        return false;
 }
 
 ////////////////////////////////////////////////////////////
