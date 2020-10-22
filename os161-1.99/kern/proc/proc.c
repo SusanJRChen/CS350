b829323a07fa40611cb2316f7a0cbb148ed70981
susachen <s529chen@uwaterloo.ca>
2020-10-22 19:47:54 -0400

    pid

--- os161-1.99/kern/proc/proc.c
+++ os161-1.99/kern/proc/proc.c
@@ -199,10 +199,12 @@ proc_destroy(struct proc *proc)
 	// remove all children, destroy the ones that are already dead
 	for (unsigned int i = array_num(proc->p_children); i > 0; i--) {
 		struct proc * cur = array_get(proc->p_children, i-1);
-		lock_acquire(cur->p_children_lk);
-		array_remove(proc->p_children, i-1);
-		cur->p_parent = NULL;
-		lock_release(cur->p_children_lk);
+		if (!cur->p_has_exited_end) {
+			lock_acquire(cur->p_children_lk);
+			array_remove(proc->p_children, i-1);
+			cur->p_parent = NULL;
+			lock_release(cur->p_children_lk);
+		}
 	}
 	// destroy or free variables
 	array_destroy(proc->p_children);
