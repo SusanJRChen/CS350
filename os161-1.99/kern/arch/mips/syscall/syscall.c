dbca6e8fce7bbe11c19723d6cf1581a91cb82021
susachen <s529chen@uwaterloo.ca>
2020-11-08 22:42:36 -0500

    execv

--- os161-1.99/kern/arch/mips/syscall/syscall.c
+++ os161-1.99/kern/arch/mips/syscall/syscall.c
@@ -117,7 +117,7 @@ syscall(struct trapframe *tf)
 	  break;
 	#if OPT_A2
     case SYS_execv:
-        err = sys_execv((char *) tf->tf_a0, (char **) tf->tf_a1, (int *) &retval);
+        err = sys_execv((char *) tf->tf_a0, (char **) tf->tf_a1);
         break;
 	case SYS_fork:
 		err = sys_fork(tf, (pid_t *)& retval);
