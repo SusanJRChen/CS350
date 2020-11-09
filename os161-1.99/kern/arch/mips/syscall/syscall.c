be9727f04ba11364bac72ae621e16e8710a56e46
susachen <s529chen@uwaterloo.ca>
2020-11-08 22:39:46 -0500

    void

--- os161-1.99/kern/arch/mips/syscall/syscall.c
+++ os161-1.99/kern/arch/mips/syscall/syscall.c
@@ -118,6 +118,7 @@ syscall(struct trapframe *tf)
 	#if OPT_A2
     case SYS_execv:
         err = sys_execv((char *) tf->tf_a0, (char **) tf->tf_a1, (int *) &retval);
+        break;
 	case SYS_fork:
 		err = sys_fork(tf, (pid_t *)& retval);
 		break;
